4801cca9a7217ebb1a74b6a4f3cda455dab1d271
Slawomir Masluk <slawomirmasluk@gmail.com>
2018-10-23 10:58:07 +0200

    Add basic structure to script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 'use strict'
 
+Start ();
 
+function Start () {
+    
+    var url = 'https://restcountries.eu/rest/v1/name/';
+    var countriesList = $('#countries');
+    $('#search').click(searchCountries);
+
+    function searchCountries() {
+        var countryName = $('#country-name').val();
+        if(!countryName.length) countryName = 'Poland';
+        $.ajax({
+            url: url + countryName,
+            method: 'GET',
+            success: showCountriesList
+        });
+  }
+
+}
