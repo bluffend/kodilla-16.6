2e1350fe539960a3b4971e3e3a7b770272a682ce
Slawomir Masluk <slawomirmasluk@gmail.com>
2018-10-23 11:15:46 +0200

    Add function showCountriesList to jscript.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 'use strict'
 
-Start ();
+Start();
 
-function Start () {
-    
+function Start() {
+
     var url = 'https://restcountries.eu/rest/v1/name/';
     var countriesList = $('#countries');
     $('#search').click(searchCountries);
 
     function searchCountries() {
         var countryName = $('#country-name').val();
-        if(!countryName.length) countryName = 'Poland';
+        if (!countryName.length) countryName = 'Poland';
         $.ajax({
             url: url + countryName,
             method: 'GET',
             success: showCountriesList
         });
-  }
+    }
+
+    function showCountriesList(resp) {
+        countriesList.empty();
+    }
 
 }
